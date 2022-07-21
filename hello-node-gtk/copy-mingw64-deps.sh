14a5b1e0e513ebd866677048deb38219a0558dd1
Dmitry Klionsky <dm.klionsky@gmail.com>
2022-07-21 19:10:49 +0300

    Add script for automatic dlls and typelibs finding

---
+++ hello-node-gtk/copy-mingw64-deps.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+mkdir -p ./lib/girepository-1.0/
+
+function copy-dlls() {
+    LOG=$1
+    SRC=$2
+    DST=$3
+    cat $LOG | sed -En 's/.*Unable to load DLL: "([^"]*\.dll)".*/\1/p' | while read dll; do
+        if [[ -f $SRC/$dll ]]; then
+            cp -v $SRC/$dll $DST/
+        fi
+    done
+}
+
+function copy-typelibs() {
+    LOG=$1
+    SRC=$2
+    DST=$3
+    cat $LOG | sed -En "s/.*Typelib file for namespace '([^']+)'.*/\1/p" | while read typelib; do
+        cp -v $SRC/$typelib* $DST/
+    done
+}
+
+TEMP=$(mktemp)
+while true; do
+    cdb -c "g;q" node index.js &>$TEMP
+    if [[ $? -ne 0 ]]; then
+	copy-dlls $TEMP /mingw64/bin/ ./
+        copy-typelibs $TEMP /mingw64/lib/girepository-1.0/ ./lib/girepository-1.0/
+    else
+        break
+    fi
+done
+rm $TEMP
+
+echo "Done"
+exit 0
