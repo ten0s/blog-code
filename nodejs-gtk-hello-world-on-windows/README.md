d81cd43f4db35d8e3edf62c86efddb205b60073c
Dmitry Klionsky <dm.klionsky@gmail.com>
2022-07-26 17:54:26 +0300

    Generalize script

--- nodejs-gtk-hello-world-on-windows/README.md
+++ nodejs-gtk-hello-world-on-windows/README.md
@@ -1,6 +1,8 @@
 Code for
-https://ten0s.github.io/blog/2022/07/22/nodejs-gtk-hello-world-on-windows and
-https://ten0s.github.io/blog/2022/07/25/find-dlls-and-typelibs-dependencies-for-nodejs-gtk-application-on-windows
+
+* https://ten0s.github.io/blog/2022/07/22/nodejs-gtk-hello-world-on-windows and
+* https://ten0s.github.io/blog/2022/07/25/find-dlls-and-typelibs-dependencies-for-nodejs-gtk-application-on-windows
+
 
 ```
 $ git clone https://github.com/ten0s/blog-code
--- nodejs-gtk-hello-world-on-windows/copy-mingw64-deps.sh
+++ nodejs-gtk-hello-world-on-windows/copy-mingw64-deps.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-mkdir -p ./lib/girepository-1.0/
+function is-sls-enabled() {
+    IMAGE=$1
+    reg query "HKLM\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Image File Execution Options\\$IMAGE" | grep -Eq "GlobalFlag\s+REG_SZ\s+0x00000002"
+}
+
+function is-cdb-available() {
+    cdb &>/dev/null
+    if [[ $? -eq 2 ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
 
 function copy-dlls() {
     LOG=$1
@@ -22,9 +34,32 @@ function copy-typelibs() {
     done
 }
 
+if [[ $# -lt 1 ]]; then
+    echo "Usage: $(basename $0) COMMAND [ARG...]"
+    exit 1
+fi
+
+COMMAND=$1
+shift
+ARGS="$@"
+
+if ! is-sls-enabled $COMMAND; then
+    echo "Enable Show Loader Snaps (sls) for $COMMAND and try again"
+    exit 1
+fi
+
+if ! is-cdb-available; then
+    echo "CDB is not found"
+    echo "Run the command below and try again"
+    echo 'export PATH="/c/Program Files (x86)/Windows Kits/10/Debuggers/x64/":$PATH'
+    exit 1
+fi
+
+mkdir -p ./lib/girepository-1.0/
+
 TEMP=$(mktemp)
 while true; do
-    cdb -c "g;q" node index.js &>$TEMP
+    cdb -c "g;q" $COMMAND $ARGS &>$TEMP
     if [[ $? -ne 0 ]]; then
         copy-dlls $TEMP /mingw64/bin/ ./
         copy-typelibs $TEMP /mingw64/lib/girepository-1.0/ ./lib/girepository-1.0/
