19d89c360edc5155256a2fedbf41d3ada874d896
Dmitry Klionsky <dm.klionsky@gmail.com>
2022-07-27 17:33:24 +0300

    Review

--- nodejs-gtk-hello-world-on-windows/index.js
+++ nodejs-gtk-hello-world-on-windows/index.js
@@ -1,4 +1,6 @@
-//process.env['PATH'] = `${__dirname}\\mingw64\\bin;` + process.env['PATH']
+//const path = require('path')
+//const rootdir = __dirname.includes('snapshot') ? path.dirname(process.argv[0]) : __dirname
+//process.env['PATH'] = `${rootdir}\\mingw64\\bin;` + process.env['PATH']
 
 const gi = require('node-gtk')
 const Gtk = gi.require('Gtk', '3.0')
